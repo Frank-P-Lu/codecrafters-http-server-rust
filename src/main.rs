c6b4557a2818d427995a639d31965ecded5aaf1a
Frank Lu <franklu.99@outlook.com>
2024-05-04 17:40:51 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
-// Uncomment this block to pass the first stage
-use std::net::TcpListener;
-use std::io::Write;
+use std::error::Error;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+
+fn process( stream: &mut TcpStream) -> Result<String, Box<dyn Error>> {
+    let mut buffer = [0; 1024];
+
+    let read_result = stream.read(&mut buffer)?;
+    let result_string = std::str::from_utf8(&buffer[..read_result])?;
+
+    // Typical request format
+    // GET /index.html HTTP/1.1
+    // Host: localhost:4221
+    // User-Agent: curl/7.64.1
+    let parts: Vec<&str> = result_string.split("\r\n").collect();
+    let first_line: Vec<&str> = parts[0].split(" ").collect();
+    let path = first_line[1];
+    match path{
+        "/" => {
+            Ok("HTTP/1.1 200 OK\r\n\r\n".to_string())
+        }
+        _ => {
+            Ok("HTTP/1.1 404 NOT FOUND\r\n\r\n".to_string())
+        }
+    }
+}
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -8,14 +31,17 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+        let mut new_stream = stream.unwrap();
+        if let Ok(response) = process(&mut new_stream) {
+            new_stream.write_all(response.as_bytes()).unwrap()
         }
+
+
+
+        println!("accepted new connection");
+
+
+
+
     }
 }
