a8d7ccc636af12bbd1677fe078d474d5606464df
Frank Lu <franklu.99@outlook.com>
2024-05-05 10:02:07 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::{env, fs, thread};
 
+fn resp201() -> String {
+    return "HTTP/1.1 201 OK\r\n\r\n".to_string();
+}
+
 fn resp200(content: &str, content_type: Option<&str>) -> String {
     let content_type = content_type.unwrap_or("text/plain");
     let content_len = content.len();
@@ -23,6 +27,7 @@ fn resp404() -> String {
 #[derive(Debug)]
 enum HttpMethod {
     GET,
+    POST,
 }
 
 #[derive(Debug)]
@@ -30,6 +35,7 @@ struct Request {
     http_method: HttpMethod,
     path: String,
     user_agent: Option<String>,
+    body: Option<String>,
 }
 
 fn parse_request(request_string: &str) -> Request {
@@ -41,28 +47,48 @@ fn parse_request(request_string: &str) -> Request {
     let lines: Vec<&str> = request_string.split("\r\n")
         .filter(|s| !s.is_empty())
         .collect();
-    let m: HashMap<&str, &str> = lines[1..].iter().map(|line|
-        line.trim().split(": ").collect()
-    ).filter(
-        |line_parts: &Vec<&str>| {
-            debug_assert!(line_parts.len() == 2, "Invalid request line: {}", line_parts[0]);
-            return line_parts.len() == 2;
-        }
-    ).map(
+    let m: HashMap<&str, &str> = lines[1..].iter().map(
+        |line|
+            line.trim().split(": ").collect())
+        .filter(
+            |line_parts: &Vec<&str>| {
+                debug_assert!(line_parts.len() == 2, "Invalid request line: {}", line_parts[0]);
+                return line_parts.len() == 2;
+            }
+        ).map(
         |line_parts|
             (line_parts[0].trim(), line_parts[1].trim())
     ).collect();
 
     let request_line: Vec<&str> = lines[0].split(" ").collect();
+    let http_method = match request_line[0] {
+        "GET" => HttpMethod::GET,
+        "POST" => HttpMethod::POST,
+        _ => panic!("Invalid HTTP method: {}", request_line[0]),
+    };
     let path = request_line[1];
+
+
     let user_agent = match m.get("User-Agent") {
         Some(ua) => Some(ua.to_string()),
         None => None,
     };
+    let body = match http_method {
+        HttpMethod::GET => None,
+        HttpMethod::POST => match m.get("Content-Length") {
+            Some(content_length) => {
+                // let content_length = content_length.parse::<usize>()?;
+                // let mut body = String::new();
+                Some(lines.last().unwrap().to_string())
+            }
+            None => None,
+        },
+    };
     return Request {
-        http_method: HttpMethod::GET,
+        http_method,
         path: path.to_string(),
         user_agent,
+        body,
     };
 }
 
@@ -101,18 +127,30 @@ fn handle(stream: &mut TcpStream, directory: Option<String>) -> Result<(), Box<d
                 }
                 "files" => {
                     let file_path = format!("{}/{}", directory.unwrap(), params[0]);
-
-                    match fs::read_to_string(file_path) {
-                        Ok(file_contents) => {
-                            resp200(file_contents.as_str(), Some("application/octet-stream"))
+                    match request.http_method {
+                        HttpMethod::GET => {
+                            match fs::read_to_string(file_path) {
+                                Ok(file_contents) => {
+                                    resp200(file_contents.as_str(), Some("application/octet-stream"))
+                                }
+                                Err(err) => {
+                                    println!("Error reading file: {}", err);
+                                    resp404()
+                                }
+                            }
                         }
-                        Err(err) => {
-                            println!("Error reading file: {}", err);
-                            resp404()
+                        HttpMethod::POST => {
+                            match fs::write(file_path, request.body.unwrap()) {
+                                Ok(_) => {
+                                    resp201()
+                                }
+                                Err(err) => {
+                                    println!("Error writing file: {}", err);
+                                    resp404()
+                                }
+                            }
                         }
                     }
-                        // .expect("Should have been able to read the file");
-
                 }
                 _ => { resp404() }
             }
